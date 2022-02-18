48f453fd2d46e0bf9d12d608d010a327b6b8aa81
Luke Hansell <luke.hansell@superawesome.com>
2022-02-18 16:31:11 +0000

    simplify terraform config

--- terraform.tf
+++ terraform.tf
@@ -6,15 +6,3 @@ resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.bucket.id
   acl = "private"
 }
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-  backend "s3" {
-    bucket = aws_s3_bucket.bucket.id
-    key    = "terraform/webapp/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
