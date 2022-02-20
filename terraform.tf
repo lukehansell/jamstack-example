7f53f122608406cbd1e46c6446aff76904141f36
Luke Hansell <lukehansell@gmail.com>
2022-02-20 16:52:27 +0000

    pass bucket name to website config

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_acl" "example" {
 }
 
 resource "aws_s3_bucket_website_configuration" "example" {
-  bucket = aws_s3_bucket.bucket
+  bucket = aws_s3_bucket.bucket.bucket
 
   index_document {
     suffix = "index.html"
