0be78ba3620a3cd802246f3bb1b350b40814ae54
Luke Hansell <luke.hansell@superawesome.com>
2022-05-13 16:40:15 +0100

    tear down infrastructure

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -1,16 +1,16 @@
-resource "aws_acm_certificate" "ssl_certificate" {
-  provider = aws.acm_provider
-  domain_name = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method = "EMAIL"
+# resource "aws_acm_certificate" "ssl_certificate" {
+#   provider = aws.acm_provider
+#   domain_name = var.domain_name
+#   subject_alternative_names = ["*.${var.domain_name}"]
+#   validation_method = "EMAIL"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  provider = aws.acm_provider
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
-  #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
+# resource "aws_acm_certificate_validation" "cert_validation" {
+#   provider = aws.acm_provider
+#   certificate_arn = aws_acm_certificate.ssl_certificate.arn
+#   #validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+# }
